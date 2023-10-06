40e8c1099338f9e13f66f7b80f02a2a6dfd6edb4
Aashish Vivekanand <aashishvanand@gmail.com>
2023-10-06 11:54:27 +0800

    Added error handling and CICD Pipeline

--- src/index.js
+++ src/index.js
@@ -20,28 +20,49 @@ function validateRegex(value, pattern, errorMessage) {
 
 function getAirportByIata(iataCode) {
     validateRegex(iataCode, /^[A-Z]{3}$/, "Invalid IATA format. Please provide a 3-letter uppercase code, e.g., 'AAA'.");
-    return airportsData.filter(airport => airport.iata === iataCode);
+    const results = airportsData.filter(airport => airport.iata === iataCode);
+    if (results.length === 0) {
+        throw new Error(`No data found for IATA code: ${iataCode}`);
+    }
+    return results;
 }
 
 function getAirportByIcao(icaoCode) {
     validateRegex(icaoCode, /^[A-Z0-9]{4}$/, "Invalid ICAO format. Please provide a 4-character uppercase code, e.g., 'NTGA'.");
-    return airportsData.filter(airport => airport.icao === icaoCode);
+    const results = airportsData.filter(airport => airport.icao === icaoCode);
+    if (results.length === 0) {
+        throw new Error(`No data found for ICAO code: ${icaoCode}`);
+    }
+    return results;
 }
 
 function getAirportByCityCode(cityCode) {
     validateRegex(cityCode, /^[A-Z]{3}$/, "Invalid City Code format. Please provide a 3-letter uppercase code, e.g., 'NYC'.");
-    return airportsData.filter(airport => airport.city_code === cityCode);
+    const results = airportsData.filter(airport => airport.city_code === cityCode);
+    if (results.length === 0) {
+        throw new Error(`No data found for City Code: ${cityCode}`);
+    }
+    return results;
 }
 
 function getAirportByCountryCode(countryCode) {
     validateRegex(countryCode, /^[A-Z]{2}$/, "Invalid Country Code format. Please provide a 2-letter uppercase code, e.g., 'US'.");
-    return airportsData.filter(airport => airport.country_code === countryCode);
+    const results = airportsData.filter(airport => airport.country_code === countryCode);
+    if (results.length === 0) {
+        throw new Error(`No data found for Country Code: ${countryCode}`);
+    }
+    return results;
 }
 
 function getAirportByContinent(continentCode) {
     validateRegex(continentCode, /^[A-Z]{2}$/, "Invalid Continent Code format. Please provide a 2-letter uppercase code, e.g., 'AS'.");
-    return airportsData.filter(airport => airport.continent_code === continentCode);
+    const results = airportsData.filter(airport => airport.continent === continentCode);
+    if (results.length === 0) {
+        throw new Error(`No data found for Continent Code: ${continentCode}`);
+    }
+    return results;
 }
+
 
 // Initially load compressed JSON data
 loadJSONData();
